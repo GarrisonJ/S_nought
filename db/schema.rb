817596218087aec432e0456d59542ec1f02b1895
Garrison Jensen <garrison.jensen@gmail.com>
2014-05-11 02:44:42 +0000

    Change title

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511023125) do
+ActiveRecord::Schema.define(version: 20140511023509) do
+
+  create_table "flags", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "flags_tickets", force: true do |t|
+  end
 
   create_table "tickets", force: true do |t|
     t.string   "title"
