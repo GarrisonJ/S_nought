10ada3a95a567239371b8ce1e03b7c5dec560a0d
Garrison Jensen <garrison.jensen@gmail.com>
2014-05-11 03:54:14 +0000

    add preview_body and small_title methods to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,9 @@
 class Ticket < ActiveRecord::Base
   has_and_belongs_to_many :flags
+  def body_preview
+    self.body[0..300] + if (self.body.length >= 300) then "..." else "" end
+  end
+  def small_title
+    self.title[0..120] + if (self.body.length >= 300) then "..." else "" end
+  end
 end
